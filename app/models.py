60e8142a24fe9fb27b09f691ee08f22117c9e247
Moureen Chepkoech <moureen.chepkoech@student.moringaschool.com>
2022-05-15 16:25:34 +0300

    define a user

---
+++ app/models.py
@@ -0,0 +1,14 @@
+from . import db
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from . import login_manager
+from datetime import datetime
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    """
+    @login_manager.user_loader Passes in a user_id to this function
+    Function queries the database and gets a user's id as a response
+    """
+    return User.query.get(int(user_id))
