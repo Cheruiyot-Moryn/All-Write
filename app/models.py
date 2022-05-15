2c59ae973614ab22b4e499b0d8be1808591a55d1
Moureen Chepkoech <moureen.chepkoech@student.moringaschool.com>
2022-05-15 16:26:28 +0300

    create a user class

--- app/models.py
+++ app/models.py
@@ -12,3 +12,35 @@ def load_user(user_id):
     Function queries the database and gets a user's id as a response
     """
     return User.query.get(int(user_id))
+
+# User Class
+class User(UserMixin, db.Model):
+    """ 
+    class model users 
+    """
+
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(255),index = True)
+    email = db.Column(db.String(255), unique=True, index=True)
+    password_hash = db.Column(db.String(255))
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    blog = db.relationship('Blog', backref='user', lazy='dynamic')
+    comment = db.relationship('Comment', backref='user', lazy='dynamic')
+    
+    @property
+    def password(self):
+        raise AttributeError("You cannot read the password attribute")
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+
+    def __repr__(self):
+        return "User: %s" %str(self.username)
